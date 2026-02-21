b4a0102a7f6fbbc7e45f35874795d6c9b585a257
chandud1124 <chandu.d1124@gmail.com>
2026-02-21 11:52:14 +0530

    Add secrets.h to repo for portability

---
+++ esp32/secrets.h
@@ -0,0 +1,12 @@
+#ifndef MINDNERO_SECRETS_H
+#define MINDNERO_SECRETS_H
+
+// Allow override via build flags or environment
+#ifndef WIFI_SSID
+#define WIFI_SSID "AIMS-WIFI"
+#endif
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "Aimswifi#2025"
+#endif
+
+#endif // MINDNERO_SECRETS_H
